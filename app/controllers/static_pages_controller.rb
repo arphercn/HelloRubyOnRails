53d2fb6537266d2f8c28e3ba68b4eedbe737088f
arpher <arpher@qq.com>
2017-01-21 21:44:51 +0800

    4 learn ruby basec,deleted contact,do nothing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
 
@@ -7,7 +8,5 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-
-  def contact
-  end
+  
 end
