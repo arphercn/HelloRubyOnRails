16c2c9411eafc0ec4c0eb6e725e88bc70c284a83
arpher <arpher@qq.com>
2017-01-24 10:52:52 +0800

    9 Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # 登入用户，然后重定向到用户的资料页面
-      log_in user
-      redirect_to user
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # 创建一个错误消息
       flash.now[:danger] = 'Invalid email/password combination' # 不完全正确
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? # 防止一个页面退出后,其他标签页点击退出,forget(current_user)报错
     redirect_to root_url
   end
 end
