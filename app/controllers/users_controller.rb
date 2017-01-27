b065d994725bf256e450fe5508f054e491547799
arpher <arpher@qq.com>
2017-01-27 09:05:38 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+     @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,7 +27,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) # 10.25
+  end
+
+  def update
+    # @user = User.find(params[:id]) # 10.25
+    if @user.update_attributes(user_params)
+      # 处理更新成功的情况
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   # 写在控制器最后,防止让其他方法无法访问
@@ -27,5 +52,25 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # 确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        store_location # 编辑用户资料页面时,储存当前url
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 确保是正确的用户
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 确保是管理员
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
