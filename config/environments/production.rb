b69975936a1b0f4094463e2b9764a23cd81c0670
arpher <arpher@qq.com>
2017-02-05 21:51:36 +0800

    hotfix change mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,11 @@ Rails.application.configure do
   host = 'mysterious-ridge-49742.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.126.com',
-    :port           => '25',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name      => 'bfengyi@126.com',
-    :password       => 'boxhezhe9',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
