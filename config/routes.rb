fa0b534ee99321160dc3f48238d9ce2eb25eaa43
arpher <arpher@qq.com>
2017-02-10 02:44:41 +0800

    14.1 Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,16 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   
   root 'static_pages#home'
 end
